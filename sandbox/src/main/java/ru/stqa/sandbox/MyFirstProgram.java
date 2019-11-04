6255e4baf5737b8f37efaad622304a420fd64a7f
YaoiIrena <yaoiirena87@gmail.com>
2019-11-04 16:35:28 +0500

    обновление - были добавлены мои созданные классы

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -6,4 +6,9 @@ public static void main(String[] arg)
 {
     System.out.println("Hello, word!");
 }
+    PointStart pointStart = new PointStart(2,3);
+    PointEnd pointEnd = new PointEnd(3,4);
+
+        System.out.println("Расстояние от точки " + pointStart.x1 + " " + pointEnd.y1 + " и " + pointStart.x2 + " " + pointEnd.y2 + " = " + Math.sqrt((pointStart.x2x1() + pointEnd.y2y1())));
+
 }
---
+++ sandbox/src/main/java/ru/stqa/sandbox/PointEnd.java
@@ -0,0 +1,18 @@
+package ru.stqa.sandbox;
+
+public class PointEnd
+{
+    public double y1;
+    public double y2;
+
+    public PointEnd(double y1, double y2)
+    {
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public double y2y1()
+    {
+        return Math.pow((this.y2 - this.y1), 2);
+    }
+}
