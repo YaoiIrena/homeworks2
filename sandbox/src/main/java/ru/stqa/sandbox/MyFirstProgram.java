4fd02718fabb653fa1658e8184f15d716b3d6677
YaoiIrena <yaoiirena87@gmail.com>
2020-01-10 17:49:11 +0500

    Revert "баловство с assertS"
    This reverts commit e94d51a96c86c853dbdc8a455d4cd8b128286cc4.

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -4,17 +4,6 @@ public class MyFirstProgram
 {
 public static void main(String[] arg)
 {
-    String s = "Hi";
-    String s1 = "Hallo";
-    String s2 = "Hallo, Welt";
-    System.out.println(LogicHW(s));
-    System.out.println(LogicHW(s1));
-    System.out.println(LogicHW(s2));
+    System.out.println("Hello, word!");
 }
-    public static boolean LogicHW(String a){
-    if (a.equals("Hallo, Welt")){
-        return true;
-    } else
-        return false;
-    }
 }
